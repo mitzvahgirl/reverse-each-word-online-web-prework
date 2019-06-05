a3674402d8108558a18003b4626239d3f2ee08f1
Amy Fogelman <abfnj1@gmail.com>
2019-06-05 11:08:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -12,3 +12,9 @@ def reverse_each_word(string)
       test_array = []
         array.collect do|string|
            test_array << string.reverse
+end
+  test_array.join(" ")
+end
+
+
+reverse_each_word("Hello there, and how are you?")          
